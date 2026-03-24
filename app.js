dbb5135e11f5d2db104928495464e6fdabe52dfd
aloehr <aloehr@users.noreply.github.com>
2026-03-24 19:07:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.body.innerHTML = "<H1>Test</H1>";
 
 setInterval( ()  => {
-  document.querySelector("H1").innerHTML = fetch("http://192.168.178.2/s").then((r) => { return r.text(); }).then((x) => { return x });
+   fetch("http://192.168.178.2/s").then((r) => { return r.text(); }).then((x) => { document.querySelector("H1").innerHTML = x });
 }, 2500);
