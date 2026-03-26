5633ba44f7981d6686656cfbfd122543df3f01ad
aloehr <aloehr@users.noreply.github.com>
2026-03-26 22:46:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 setInterval( ()  => {
+   document.title = "StromMonitor 0.1";
+   
    fetch("http://192.168.178.2/s").then((r) => { return r.text(); }).then((x) => {
       d = x.trimEnd().split(" ");
       da = new Date(+d[1] * 1000);
+      
       document.body.innerHTML ="<p>Pulse ID: " + d[0] + 
          "; Zeit: " + da.getHours() + ":" da.getMinutes() + ":" + da.getSeconds() + 
          " Uhr; Pulse Länge: " + d[2] + 
@@ -10,4 +13,4 @@ setInterval( ()  => {
          " V; Max Pulse Volts: " + d[5] + 
          " V; Verbrauch: " (3600000 / +d[3]) + " kWh</p>" + document.body.innerHTML; 
    });
-}, 2500);
+}, 5000);
