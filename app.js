b47417503632a9a6813ab532fa4cdbb0079cbfa7
aloehr <aloehr@users.noreply.github.com>
2026-03-26 12:47:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 setInterval( ()  => {
    fetch("http://192.168.178.2/s").then((r) => { return r.text(); }).then((x) => {
-      d = x.split(" ");
+      d = x.trimEnd().split(" ");
       
-      document.body.innerHTML +="<p>" + d.join("; ") + "</p"; 
+      document.body.innerHTML ="<p>" + d.join("; ") + "</p>" + document.body.innerHTML; 
    });
 }, 2500);
