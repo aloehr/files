bafdd5f4e9099b7dc72f24d12222bd6e66867b03
aloehr <aloehr@users.noreply.github.com>
2026-03-26 22:48:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ setInterval( ()  => {
       d = x.trimEnd().split(" ");
       da = new Date(+d[1] * 1000);
       
-      document.body.innerHTML ="<p>Pulse ID: " + d[0] + 
-         "; Zeit: " + da.getHours() + ":" da.getMinutes() + ":" + da.getSeconds() + 
+      document.body.innerHTML = "<p>Pulse ID: " + d[0] + 
+         "; Zeit: " + da.getHours() + ":" + da.getMinutes() + ":" + da.getSeconds() + 
          " Uhr; Pulse Länge: " + d[2] + 
          " ms; Pulse Intervall: " + d[3] + 
          " ms; Trigger Delta: " + d[4] + 
          " V; Max Pulse Volts: " + d[5] + 
-         " V; Verbrauch: " (3600000 / +d[3]) + " kWh</p>" + document.body.innerHTML; 
+         " V; Verbrauch: "  + (3600000 / +d[3]) + " kWh</p>" + document.body.innerHTML; 
    });
 }, 5000);
