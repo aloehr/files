58a9f42b458c8dd519442f323cfe31828b933e8f
aloehr <aloehr@users.noreply.github.com>
2026-03-26 22:45:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 setInterval( ()  => {
    fetch("http://192.168.178.2/s").then((r) => { return r.text(); }).then((x) => {
       d = x.trimEnd().split(" ");
-      
-      document.body.innerHTML ="<p>" + d.join("; ") + "</p>" + document.body.innerHTML; 
+      da = new Date(+d[1] * 1000);
+      document.body.innerHTML ="<p>Pulse ID: " + d[0] + 
+         "; Zeit: " + da.getHours() + ":" da.getMinutes() + ":" + da.getSeconds() + 
+         " Uhr; Pulse Länge: " + d[2] + 
+         " ms; Pulse Intervall: " + d[3] + 
+         " ms; Trigger Delta: " + d[4] + 
+         " V; Max Pulse Volts: " + d[5] + 
+         " V; Verbrauch: " (3600000 / +d[3]) + " kWh</p>" + document.body.innerHTML; 
    });
 }, 2500);
